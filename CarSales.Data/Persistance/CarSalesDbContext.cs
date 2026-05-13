edf62d71e7b103696d70739ce858669659c1e2b6
Божидар Петков <xidz01bog@gmail.com>
2026-05-13 16:00:54 +0300

    Added db context

---
+++ CarSales.Data/Persistance/CarSalesDbContext.cs
@@ -0,0 +1,55 @@
+﻿using CarSales.Data.DataConstraints;
+using CarSales.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSales.Data.Persistance
+{
+    public class CarSalesDbContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Favourite> Favourites { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<User> Users { get; set; }
+        public CarSalesDbContext(DbContextOptions<CarSalesDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region User
+            modelBuilder.Entity<User>()
+                .HasData(new User
+                {
+                    Id = Guid.NewGuid(),
+                    Username = "admin",
+                    Password = "admin",
+                    FirstName = "Admin",
+                    LastName = "Admin",
+                    Age = 1,
+                    IsAdmin = true,
+                });
+            #endregion
+
+            #region Favourite
+            modelBuilder.Entity<Favourite>()
+                .HasKey(f => new { f.UserId, f.CarId });
+
+            modelBuilder.Entity<Favourite>()
+                .HasOne(f => f.User)
+                .WithMany(f => f.Favourites)
+                .HasForeignKey(f => f.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Favourite>()
+                .HasOne(f => f.Car)
+                .WithMany(f => f.Favourites)
+                .HasForeignKey(f => f.CarId)
+                .OnDelete(DeleteBehavior.Restrict);
+            #endregion
+        }
+    }
+}

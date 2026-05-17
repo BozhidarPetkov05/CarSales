e7e49a54e78170ceb4e405d16971307b671b67a6
Божидар Петков <xidz01bog@gmail.com>
2026-05-17 22:27:33 +0300

    Added UserController

---
+++ CarSales/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using CarSales.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _userService.GetAllAsync();
+
+            return Ok(users);
+        }
+    }
+}
